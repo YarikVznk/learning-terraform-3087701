5c469328a1eca4d595fc0934147a4e542e233d9a
YarikVznk <yarik.vznk@gmail.com>
2026-04-24 14:54:23 +0300

    blog AlB

--- main.tf
+++ main.tf
@@ -66,9 +66,7 @@ module "blog_alb" {
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
 
-  security_groups = [module.blog_vpc.security_group_id]
-
- 
+  security_groups = [module.blog_sg.security_group_id]
 
   listeners = {
     blog-http = {
@@ -81,8 +79,6 @@ module "blog_alb" {
     }
     
   }
-
- 
 
   tags = {
     Environment = "Dev"
