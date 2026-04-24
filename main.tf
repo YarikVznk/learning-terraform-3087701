9ba05bf7332421049023604ba553fd881e54bd5e
YarikVznk <yarik.vznk@gmail.com>
2026-04-24 16:00:07 +0300

    instaled httpd to clean instance

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ module "blog_vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
+  map_public_ip_on_launch = true
 
   tags = {
     Terraform = "true"
@@ -83,6 +84,15 @@ launch_template_name = "blog"
 security_groups      = [module.blog_sg.security_group_id]
 instance_type        = var.instance_type
 image_id             = data.aws_ssm_parameter.amazon_linux_2023.value
+
+user_data = base64encode(<<-EOF
+#!/bin/bash
+dnf update -y
+dnf install -y httpd
+systemctl enable --now httpd
+echo "Hello from AL2023 behind ALB" > /var/www/html/index.html
+EOF
+)
 
 traffic_source_attachments = {
   blog_alb = {
