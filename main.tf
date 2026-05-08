72beb526218aba629c54b56202f0b6335e86dc21
YarikVznk <yarik.vznk@gmail.com>
2026-05-08 18:25:55 +0300

    refactor to use variables

--- main.tf
+++ main.tf
@@ -1,23 +1,35 @@
-data "aws_ssm_parameter" "amazon_linux_2023" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = [var.ami_filter.name]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = [var.ami_filter.owner] # Bitnami
 }
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["${var.environment.network_prefix}.1.0/24", "${var.environment.network_prefix}.2.0/24", "${var.environment.network_prefix}.3.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = true
   map_public_ip_on_launch = true
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -58,7 +70,7 @@ module "blog_alb" {
   }
 
   tags = {
-    Environment = "Dev"
+    Environment = var.environment.name
 
   }
 }
@@ -75,24 +87,17 @@ module "blog_autoscaling" {
 source   = "terraform-aws-modules/autoscaling/aws"
 version  = "9.2.0"
 name     = "blog"
-min_size = 1
-max_size = 2
+min_size = var.min_size
+max_size = var.max_size
 
 vpc_zone_identifier = module.blog_vpc.public_subnets
 
 launch_template_name = "blog"
 security_groups      = [module.blog_sg.security_group_id]
 instance_type        = var.instance_type
-image_id             = data.aws_ssm_parameter.amazon_linux_2023.value
+image_id             = data.aws_ami.app_ami.id
 
-user_data = base64encode(<<-EOF
-#!/bin/bash
-dnf update -y
-dnf install -y httpd
-systemctl enable --now httpd
-echo "Hello from AL2023 behind ALB" > /var/www/html/index.html
-EOF
-)
+
 
 traffic_source_attachments = {
   blog_alb = {
