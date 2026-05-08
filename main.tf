5e034f28869d8c988ebac5db5b6666ed4ba229e7
YarikVznk <yarik.vznk@gmail.com>
2026-05-08 18:20:23 +0300

    refactor to use variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,56 +11,98 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["${var.environment.network_prefix}.1.0/24", "${var.environment.network_prefix}.2.0/24", "${var.environment.network_prefix}.3.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
-resource "aws_security_group" "blog" {
-  name = "blog"
+  enable_nat_gateway = true
+  map_public_ip_on_launch = true
+
   tags = {
     Terraform = "true"
+    Environment = var.environment.name
   }
-  vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+module "blog_sg" {
+source  = "terraform-aws-modules/security-group/aws"
+version = "5.3.1"
+name    = "blog_new"
+
+vpc_id = module.blog_vpc.vpc_id
+
+ingress_rules       = ["http-80-tcp","https-443-tcp"]
+ingress_cidr_blocks = ["0.0.0.0/0"]
+
+egress_rules       = ["all-all"]
+egress_cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  security_groups = [module.blog_sg.security_group_id]
+
+  listeners = {
+    blog-http = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_arn = aws_lb_target_group.blog.arn
+ 
+      }
+    }
+    
+  }
+
+  tags = {
+    Environment = var.environment.name
+
+  }
+}
+
+resource "aws_lb_target_group" "blog" {
+  name     = "blog"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.blog_vpc.vpc_id
 }
 
 
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
-}
+module "blog_autoscaling" {
+source   = "terraform-aws-modules/autoscaling/aws"
+version  = "9.2.0"
+name     = "blog"
+min_size = var.min_size
+max_size = var.max_size
+
+vpc_zone_identifier = module.blog_vpc.public_subnets
+
+launch_template_name = "blog"
+security_groups      = [module.blog_sg.security_group_id]
+instance_type        = var.instance_type
+image_id             = data.aws_ami.app_ami.id
 
 
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+
+traffic_source_attachments = {
+  blog_alb = {
+    traffic_source_identifier = aws_lb_target_group.blog.arn
+  }
 }
+}
+
--- variables.tf
+++ variables.tf
@@ -2,3 +2,36 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description "Name filter and owner for AMI"
+  type = object ({
+    name  = string
+    owner = string  
+  })
+  default = {
+    name   = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    owners = ["979382823631"] # Bitnami
+  }
+}
+
+variable "environment"{
+  description = "Deployment environment"
+  type        = object ({
+    name           = string
+    network_prefix = string
+  })
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
+}
+
+variable "min_size"{
+  description = "Minimum size of instances in ASG"
+  default     = 1
+}
+
+variable "max_size"{
+  description = "Maximum size of instances in ASG"
+  default     = 2
+}
